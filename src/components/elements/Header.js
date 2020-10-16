693de66c61796a529ea87ae66c33c3549d8ffa7f
Coco <coco.li7777@gmail.com>
2020-10-16 23:32:38 +1100

    new ui design

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -2,13 +2,63 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/styles';
-import { AppBar, Toolbar } from '@material-ui/core';
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  Icon,
+  IconButton,
+  ButtonGroup,
+  Grid,
+} from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     background:
       'linear-gradient(90deg, rgba(168,230,19,1) 0%, rgba(0,255,205,1) 34%, rgba(30,140,94,1) 100%)',
+  },
+  btnGroup: {
+    width: '100%',
+    marginTop: '10px',
+  },
+  btnItem: {
+    fontFamily: 'Roboto',
+    fontWeight: 300,
+    width: '40%',
+    height: '40px',
+    backgroundColor: 'transparent',
+    fontSize: 20,
+    color: 'white',
+    borderColor: 'red',
+    '&:hover': {
+      backgroundColor: '#a5cf00',
+      border: 'none',
+    },
+  },
+  iconBtn: {
+    marginTop: '10px',
+
+    //border: '1px red solid',
+  },
+  btn: {
+    fontFamily: 'Roboto',
+    fontWeight: 'bold',
+    backgroundColor: 'transparent',
+    borderColor: 'white',
+    color: 'white',
+    width: '100px',
+    height: '40px',
+    marginTop: '10px',
+    borderRadius: '5%',
+    fontSize: 17,
+    '&:hover': {
+      backgroundColor: '#a5cf00',
+      border: 'none',
+    },
+  },
+  item: {
+    textAlign: 'center',
   },
 }));
 
@@ -22,12 +72,38 @@ function Header() {
       data-test="headerComponent"
     >
       <Toolbar>
-        <img
-          src="/malih.png"
-          alt="logo"
-          style={{ width: '140px' }}
-          data-test="logoIMG"
-        />
+        <Grid container spacing={2}>
+          <Grid item sm={7} xs={12}>
+            <img
+              src="/malih.png"
+              alt="logo"
+              style={{ width: '220px', height: '40px' }}
+              className={classes.iconBtn}
+              data-test="logoIMG"
+            />
+          </Grid>
+          <Grid item sm={4} xs={12} className={classes.item}>
+            <ButtonGroup
+              disableElevation
+              variant="contained"
+              className={classes.btnGroup}
+            >
+              <Button className={classes.btnItem}>Home</Button>
+              <Button className={classes.btnItem}>Profile</Button>
+              <Button className={classes.btnItem}>Company</Button>
+            </ButtonGroup>
+          </Grid>
+          <Grid item sm={1} xs={12} className={classes.item}>
+            <Button
+              variant="outlined"
+              color="primary"
+              href="#outlined-buttons"
+              className={classes.btn}
+            >
+              LOG IN
+            </Button>
+          </Grid>
+        </Grid>
       </Toolbar>
     </AppBar>
   );
